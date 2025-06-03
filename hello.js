7690f1599d57d0db4d783c73288f33455b28915d
karls77 <karlschmit@gmail.com>
2025-06-03 12:22:17 -0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,12 @@
 alert("Hello world!");
+var nodeList = document.querySelectorAll("div.items-desc");
+for (var i = 0, length = nodeList.length; i < length; i++) {
+    //  nodeList[i].innerHTML = "Soup of the day is: Soup Dujour";
+
+     // Create a new div element
+    const newDiv = document.createElement("p");
+    // Optionally, add some content or attributes to the new div
+    newDiv.innerHTML = "Soup of the day is: Soup du jour";
+    // newDiv.className = "new-class";
+    nodeList[i].appendChild(newDiv);
+  }
